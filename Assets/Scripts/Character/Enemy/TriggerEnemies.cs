ac88bd2870b21623a5cca7871351e4a6dbb58ddb
talhaucarr <talhaucarr@hotmail.com>
2021-08-31 18:01:24 +0300

    Added trigger enemies

---
+++ Assets/Scripts/Character/Enemy/TriggerEnemies.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Core;
+using Character.Enemy;
+
+public class TriggerEnemies : MonoBehaviour
+{
+    [SerializeField] private GameObject[] enemies;
+
+    private BoxCollider _collider;
+
+    private void Start()
+    {
+        _collider = GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _collider.enabled = false;
+         TriggerAllEnemies();
+    }
+
+    private void TriggerAllEnemies()
+    {
+        foreach (var enemy in enemies)
+        {
+            enemy.GetComponent<AIController>().enabled = true;
+        }
+    }
+}
